f7ebde2a5d1c9d7d7e9242ddbfa23084dc8ea387
Wonseok Shin <wsshin97@gmail.com>
2023-09-22 00:05:13 -0400

    Commit changes

--- test_libgit2.jl
+++ test_libgit2.jl
@@ -5,13 +5,20 @@ repo = LibGit2.GitRepo(repo_path)
 LibGit2.add!(repo, ".")
 LibGit2.commit(repo, "Commit changes")
 
+# Try to mimic LibGit2.jl > function push(repo; ...), which tries to automatize a lot.
+
+
 rmt = LibGit2.lookup_remote(repo, "origin")
-options = LibGit2.PushOptions(callbacks=LibGit2.RemoteCallbacks())
 
+sshcreds = LibGit2.SSHCredential()
+sshcreds.user = "wsshin"
+sshcreds.prvkey = joinpath(ENV["HOME"], ".ssh", "id_rsa")
+sshcreds.pubkey = joinpath(ENV["HOME"], ".ssh", "id_rsa.pub")
 
-# sshcreds = LibGit2.SSHCredential()
-# sshcreds.user = "wsshin"
-# sshcreds.prvkey = joinpath(ENV["HOME"], ".ssh", "id_rsa")
-# sshcreds.pubkey = joinpath(ENV["HOME"], ".ssh", "id_rsa.pub")
+cred_payload = reset!(CredentialPayload(sshcreds), GitConfig(repo))
+callbacks[:credentials] = (credentials_cb(), cred_payload)
 
-LibGit2.push(rmt, ["refs/heads/main"]; options)
+remote_callbacks = RemoteCallbacks(callbacks)
+push_opts = PushOptions(callbacks=remote_callbacks)
+
+LibGit2.push(rmt, ["refs/heads/main"], force=force, options=push_opts)
