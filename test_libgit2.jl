ed49d62736f39910fcde9d38e4dafb91c3c9bfad
Wonseok Shin <wsshin97@gmail.com>
2023-09-21 23:47:20 -0400

    Commit changes

--- test_libgit2.jl
+++ test_libgit2.jl
@@ -6,5 +6,13 @@ LibGit2.add!(repo, ".")
 LibGit2.commit(repo, "Commit changes")
 
 rmt = LibGit2.lookup_remote(repo, "origin")
-options = LibGit2.PushOptions(callbacks=LibGit2.credentials_cb())
+LibGit2.add_push!(repo, rmt, "refs/heads/main")
+options = LibGit2.PushOptions()
+
+
+# sshcreds = LibGit2.SSHCredential()
+# sshcreds.user = "wsshin"
+# sshcreds.prvkey = joinpath(ENV["HOME"], ".ssh", "id_rsa")
+# sshcreds.pubkey = joinpath(ENV["HOME"], ".ssh", "id_rsa.pub")
+
 LibGit2.push(rmt, ["refs/heads/main"]; options)
