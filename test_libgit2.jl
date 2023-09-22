cb1021c654a744a0e1088c07edba29f524c3bc65
Wonseok Shin <wsshin97@gmail.com>
2023-09-21 22:42:39 -0400

    Commit changes

--- test_libgit2.jl
+++ test_libgit2.jl
@@ -4,4 +4,6 @@ repo_path = joinpath(ENV["JULIA_PKG_DEVDIR"], "LibGit2Test")
 repo = LibGit2.GitRepo(repo_path)
 LibGit2.add!(repo, ".")
 LibGit2.commit(repo, "Commit changes")
-LibGit2.push(repo)
+
+remote = LibGit2.GitRemoteAnon(repo, "git@github.com:wsshin/LibGit2Test.git")
+LibGit2.push(remote, "refs/heads/main")
